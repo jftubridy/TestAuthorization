6f6590c5ffbc15b6d5aec6b8b96a3e583c31c6d1
Student <example@example.com>
2019-08-26 14:48:06 -0700

    partially kind of working authorization

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestAuthentication.Data;
+
+[assembly: HostingStartup(typeof(TestAuthentication.Areas.Identity.IdentityHostingStartup))]
+namespace TestAuthentication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Data/Migrations/20190826211732_initAuth.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestAuthentication.Data.Migrations
+{
+    public partial class initAuth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
