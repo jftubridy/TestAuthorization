22763fb0859be8f99a76ce36d37f4364a9631d80
Student <example@example.com>
2019-08-26 15:32:51 -0700

    privacy page works after using [Authorize]

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TestAuthentication.Pages
 {
+    [Authorize]
+
     public class PrivacyModel : PageModel
     {
         public void OnGet()
